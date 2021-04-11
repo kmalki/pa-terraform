c28a414ee53487751865d841e5391cd1a77c791c
Kamel Malki <kamelmalk96@gmail.com>
2021-04-11 23:06:57 +0200

    tf firestore

---
+++ firestore.tf
@@ -0,0 +1,15 @@
+resource "google_filestore_instance" "instance" {
+  name = var.firestore.name
+  zone = var.zone
+  tier = var.firestore.tier
+
+  file_shares {
+    capacity_gb = var.firestore.file_share.capacity
+    name        = var.firestore.file_share.name
+  }
+
+  networks {
+    network = var.firestore.network.network
+    modes   = var.firestore.network.modes
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -3,3 +3,10 @@ project_id = "projetannuel-309416"
 buckets = {
   "test" = {name = "test-bucket", class = "STANDARD"}
 }
+
+firestore = {
+  name = "firestore-main",
+  tier = "STANDARD",
+  file_share = {name = "share-main", capacity = 1024},
+  network = {network = "default", modes = ["MODE_IPV4"]}
+}
